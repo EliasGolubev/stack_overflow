95b2d13195b87987d7bf1b6ea231ae72579c13e8
Elias <igolubevssmnu@mail.ru>
2017-03-06 22:32:01 +0300

    Create test association

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
+  context 'Associations' do 
+    it { should have_many :answers }
+  end
+
   context 'Validations' do
     it { should validate_presence_of :title }
     it { should validate_presence_of :body }
