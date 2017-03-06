057a409359536997dee2ef9bee4e6c417682dc8e
Elias <igolubevssmnu@mail.ru>
2017-03-06 23:51:44 +0300

    Delete a question deleted answers

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,12 @@ class Question < ApplicationRecord
   has_many :answers
 
   validates :title, :body, presence: true
+
+  after_destroy :destroy_answers
+
+  private
+
+  def destroy_answers
+    answers.each(&:destroy)
+  end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -9,4 +9,14 @@ RSpec.describe Question, type: :model do
     it { should validate_presence_of :title }
     it { should validate_presence_of :body }
   end
+
+  context 'Collbacks' do 
+    it 'delete a question deleted answers' do 
+      question = Question.create(title: '123', body: '321')
+      answer = Answer.create(body: '4321', question: question)
+      question.destroy
+
+      expect(Answer.all).not_to include answer 
+    end 
+  end
 end
