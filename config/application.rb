99b1523454721b911345b0fe4f87643147ad2708
Elias <igolubevssmnu@mail.ru>
2017-03-08 00:13:33 +0300

    Config test generators rspec

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,17 @@ module StackOverflow
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Configurate generation rspec
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: true,
+                       view_spec: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       request_specs: false,
+                       controller_spec: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
   end
 end
