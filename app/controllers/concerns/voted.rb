0d9ce75a1ffd82dd1df7085a9bd6be47ab3d8859
Elias <igolubevssmnu@mail.ru>
2017-04-10 11:12:42 +0300

    Refactoring controller Voted

--- app/controllers/concerns/voted.rb
+++ app/controllers/concerns/voted.rb
@@ -3,16 +3,14 @@ module Voted
 
   included do
     before_action :load_votable, only: [:positive_vote, :negative_vote, :re_vote]
+    before_action :author_votable_vote, only: [:positive_vote, :negative_vote]
+    before_action :author_votable_re_vote, only: [:re_vote]
   end
 
   def positive_vote
     if @votable.user_vote?(current_user)
-      if !current_user || current_user.author?(@votable)
-        render json: { id: @votable.id, message: 'You can\'t vote' }, status: :unprocessable_entity
-      else
-        @votable.positive(current_user)
-        render json: { id: @votable.id, rating: @votable.rating }
-      end
+      @votable.positive(current_user)
+      render json: { id: @votable.id, rating: @votable.rating }
     else
       render json: { id: @votable.id, message: 'You can vote once' }, status: :unprocessable_entity
     end
@@ -20,31 +18,35 @@ module Voted
 
   def negative_vote
     if @votable.user_vote?(current_user)
-      if !current_user || current_user.author?(@votable)
-        render json: {id: @votable.id, message: 'You can\'t vote' }, status: :unprocessable_entity
-      else
-        @votable.negative(current_user)
-        render json: { id: @votable.id, rating: @votable.rating }
-      end
+      @votable.negative(current_user)
+      render json: { id: @votable.id, rating: @votable.rating }
     else
       render json: { id: @votable.id, message: 'You can vote once' }, status: :unprocessable_entity
     end
   end
 
   def re_vote
-    if !current_user || current_user.author?(@votable)
-      render json: { id: @votable.id, message: 'You can\'t re-vote' }, status: :unprocessable_entity
+    if @votable.user_vote?(current_user)
+      render json: { id: @votable.id, message: 'You must vote' }, status: :unprocessable_entity
     else
-      if @votable.user_vote?(current_user)
-        render json: { id: @votable.id, message: 'You must vote' }, status: :unprocessable_entity
-      else
-        @votable.re_vote(current_user)
-        render json: { id: @votable.id, rating: @votable.rating }
-      end
+      @votable.re_vote(current_user)
+      render json: { id: @votable.id, rating: @votable.rating }
     end
   end
 
   private
+
+  def author_votable_vote
+    if !current_user || current_user.author?(@votable)
+      render json: { id: @votable.id, message: 'You can\'t vote' }, status: :unprocessable_entity
+    end
+  end
+
+  def author_votable_re_vote
+    if !current_user || current_user.author?(@votable)
+      render json: { id: @votable.id, message: 'You can\'t re-vote' }, status: :unprocessable_entity
+    end
+  end
 
   def load_votable
     @votable = controller_name.classify.constantize.find(params[:id])
