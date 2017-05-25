1390b2885afd0b952bbe9afe6ac8b42098763db3
Elias <igolubevssmnu@mail.ru>
2017-05-25 14:54:52 +0300

    Add gem

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module StackOverflow
 
     ActiveModelSerializers.config.adapter = :json
 
+    config.active_job.queue_adpter = :sidekiq
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
