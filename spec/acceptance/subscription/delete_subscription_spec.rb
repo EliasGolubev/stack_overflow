a81a91ef963d7e59f512789ff2bafa737328d205
Elias <igolubevssmnu@mail.ru>
2017-05-28 16:07:17 +0300

    fixed

--- spec/acceptance/subscription/delete_subscription_spec.rb
+++ spec/acceptance/subscription/delete_subscription_spec.rb
@@ -4,6 +4,15 @@ feature 'Delete subscriptions with question' do
   given(:user) { create(:user) }
   given(:question){ create(:question) }
 
+  context 'non authenticated user' do 
+    scenario 'can\'t unsubscribe the question' do
+      visit question_path(question)
+      within '.question-show' do 
+        expect(page).to_not have_link 'Unsubscribe'
+      end
+    end 
+  end
+
   context 'with user subscribe question' do 
     given!(:subscription){ create(:subscription, question: question, user: user) }
 
@@ -14,6 +23,16 @@ feature 'Delete subscriptions with question' do
         expect(page).to have_link 'Unsubscribe'
         click_on 'Unsubscribe'
         expect(page).to have_link 'Subscribe'
+      end
+    end
+  end
+
+  context 'with user non subscribe question' do 
+    scenario 'can\'t unsubscribe the question' do
+      sign_in user
+      visit question_path(question)
+      within '.question-show' do 
+        expect(page).to_not have_link 'Unsubscribe'
       end
     end
   end
