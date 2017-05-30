8b277d19d28850a52646f3d65a56c3862b4e285b
Elias <igolubevssmnu@mail.ru>
2017-05-30 22:36:19 +0300

    Fixed jobs n + 1 problem

--- app/jobs/author_notice_job.rb
+++ app/jobs/author_notice_job.rb
@@ -2,8 +2,7 @@ class AuthorNoticeJob < ApplicationJob
   queue_as :default
 
   def perform(answer)
-    subscriptions = answer.question.subscriptions
-    subscriptions.find_each do |subscription|
+    answer.question.subscriptions.includes(:user).find_each do |subscription|
       AnswerNoticeMailer.answer_notice(subscription.user, answer).deliver_later if answer.user_id != subscription.user_id
     end
   end
--- spec/support/sphinx.rb
+++ spec/support/sphinx.rb
@@ -12,6 +12,12 @@ end
 
 RSpec.configure do |config|
   config.include SphinxHelpers, type: :feature
+  config.include SphinxHelpers, type: :sphinx
+
+  config.before(:each, type: :sphinx) do
+    DatabaseCleaner.strategy = :deletion
+  end
+
 
   config.before(:suite) do
     # Ensure sphinx directories exist for the test environment
@@ -19,10 +25,5 @@ RSpec.configure do |config|
     # Configure and start Sphinx, and automatically
     # stop Sphinx at the end of the test suite.
     ThinkingSphinx::Test.start_with_autostop
-  end
-
-  config.before(:each) do
-    # Index data when running an acceptance spec.
-    index if example.metadata[:js]
   end
 end
