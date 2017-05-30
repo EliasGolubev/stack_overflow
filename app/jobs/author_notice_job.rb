16264872bbb22697e267432d918a14f72e9e9fd7
Elias <igolubevssmnu@mail.ru>
2017-05-30 14:01:12 +0300

    Fixed

--- app/jobs/author_notice_job.rb
+++ app/jobs/author_notice_job.rb
@@ -2,7 +2,8 @@ class AuthorNoticeJob < ApplicationJob
   queue_as :default
 
   def perform(answer)
-    answer.question.subscriptions.find_each do |subscription|
+    subscriptions = answer.question.subscriptions
+    subscriptions.find_each do |subscription|
       AnswerNoticeMailer.answer_notice(subscription.user, answer).deliver_later if answer.user_id != subscription.user_id
     end
   end
